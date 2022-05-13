cb8cb91c3aabdffca7711c09f30b6ae7225d61dd
sahidulcse12 <m.sahidulislam.14@gmail.com>
2022-05-14 00:28:13 +0600

    jquery done

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,40 @@ $(document).ready(function () {
         time: 1000
     });
 
+    $('.testimonial').slick({
+        slidesToShow: 3,
+        prevArrow:
+            '<button type="button" class="slick-prev"><i class="fas fa-long-arrow-alt-left"></i></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><i class="fas fa-long-arrow-alt-right"></i></button>',
+        dots: true,
+        infinite: true,
+        loop: true,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+
 })
 
 $(window).on('scroll', function () {
